4ddad884d0a9fd95cea1c9ac5f3d0ecb2c111690
ibti57 <105206384+ibti57@users.noreply.github.com>
2022-05-19 15:49:58 +0200

    Update pp.c

--- pp.c
+++ pp.c
@@ -3,23 +3,10 @@
 
 voir les fonctions dans le fichier du projet_info 
 
-
- int menu(){
-
- 		printf("Bienvenue dans votre location de voiture .\n\n");
- 		printf("***Menu***");
- 		printf("1.Se connecter \n");
- 		printf("2.Créer un compte \n");
- 		
- 		
- 		
- 		
- 		
- 		
+ 		 		 			
 int main(){
 	printf("*********************************************\n");
 	printf("           Application CY-Loca               \n");
 	printf("*********************************************\n");
 	
-	
-
+	//mettre les fonctions dans l'ordre quand elles seront toutes finies
